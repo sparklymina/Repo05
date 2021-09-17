e9572972abc32f467191b055a677b32639170331
sparklymina <ecausevic93@gmail.com>
2021-09-17 13:43:44 -0400

    adding ex14

---
+++ ex14/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+    // only change code below this line
+    var myNoun = "dog";
+    var myVerb = "ran";
+    var myAdjective = "big";
+    var myAdverb = "quickly";
+    var wordBlanks = "The " + myAdjective + " red " + myNoun + " " + myVerb + " " + myAdverb + " into a pole.";
+    // only change code above this line
+    return wordBlanks;
+}
+
+console.log(main());
+module.exports = main;
