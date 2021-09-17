7f578fc80293bd21fc341593536f95006d21a7bf
sparklymina <ecausevic93@gmail.com>
2021-09-17 13:17:27 -0400

    adding ex08

---
+++ ex08/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+    // only change code below this line
+    var lastName = "Doe";
+    var lastNameLength;
+    lastNameLength = lastName.length
+    // only change code above this line
+    return lastNameLength;
+}
+
+console.log(main());
+module.exports = main;
